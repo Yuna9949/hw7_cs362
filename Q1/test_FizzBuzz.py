49ef6d722ca0debf3ff5d3112a043d43db96f673
Yuna Oh <ustina0409@gmail.com>
2021-06-01 10:41:26 -0700

    Wrote second test, to get output as 'Buzz'

--- Q1/test_FizzBuzz.py
+++ Q1/test_FizzBuzz.py
@@ -5,5 +5,8 @@ class TestFizzBuzz(unittest.TestCase):
     def test_FizzBuzz_3(self):
         self.assertEqual(FizzBuzz.FizzBuzz(3),'Fizz')
 
+    def test_FizzBuzz_5(self):
+        self.assertEqual(FizzBuzz.FizzBuzz(5),'Buzz')
+
 if __name__ == '__main__':
     unittest.main()
