1f0a4cc547e605af716b075fae0f5c53cbce8039
Yuna Oh <ustina0409@gmail.com>
2021-06-01 11:06:26 -0700

    Wrote second test, to get output as false while input is evenly divisible by 100 even if input is evenly divisible by 4

--- Q2/LeapYear.py
+++ Q2/LeapYear.py
@@ -3,3 +3,12 @@ def leapyear(y):
     if y % 4 == 0:
         LYChk = True
     return LYChk
+
+
+#y = int(input('Input year : '))
+#if leapyear(y):
+#    print(y, end='')
+#    print(' is a leap year.')
+#else :
+#    print(y, end='')
+#    print(' is not a leap year.')
--- Q2/test_LeapYear.py
+++ Q2/test_LeapYear.py
@@ -4,5 +4,8 @@ class TestLeapYear(unittest.TestCase):
     def test_LeapYear_4(self):
         self.assertEqual(LeapYear.LeapYear(2012), True)
 
+    def test_LeapYear_100(self):
+        self.assertEqual(LeapYear.LeapYear(2100), False)
+
 if __name__=='__main__':
     unittest.main
